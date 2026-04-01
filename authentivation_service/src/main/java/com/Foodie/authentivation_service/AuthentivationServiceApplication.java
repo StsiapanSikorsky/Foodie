4be45e7a22f2da17a736433d9286a7078e5f6809
Stsiapan Sikorsky <stepa360360@gmail.com>
2026-04-01 20:49:19 +0300

    init commit

---
+++ authentivation_service/src/main/java/com/Foodie/authentivation_service/AuthentivationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Foodie.authentivation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentivationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthentivationServiceApplication.class, args);
+	}
+
+}
---
+++ authentivation_service/src/test/java/com/Foodie/authentivation_service/AuthentivationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Foodie.authentivation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthentivationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
